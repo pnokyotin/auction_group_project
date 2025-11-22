dcac1c5bd859314e992572a0ecfabda6d21073cd
pnokyotin <peerapat.noky@bumail.net>
2025-11-23 03:05:32 +0700

    ปรับ layout ของ card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Main from "./pages/Main";
+import InsertProduct from "./pages/InsertProduct";
 import PrivateRoute from "./components/PrivateRoute";
 import Navbar from "./components/Navbar";
 
@@ -26,7 +27,18 @@ function App() {
           }
         />
 
-        {/* redirect / ไป login */}
+        {/* หน้า InsertProduct ต้อง login ก่อน */}
+        <Route
+          path="/insert-product"
+          element={
+            <PrivateRoute>
+              <Navbar />
+              <InsertProduct />
+            </PrivateRoute>
+          }
+        />
+
+        {/* redirect / ไป /main */}
         <Route path="/" element={<Navigate to="/main" replace />} />
       </Routes>
     </Router>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,21 +1,23 @@
 export default function Card({ title, description, image }) {
   return (
-    <div className="flex-shrink-0 bg-white/10 backdrop-blur-md rounded-lg shadow-lg hover:shadow-[0_0_20px_rgba(255,86,246,0.6)] transition-shadow duration-300 overflow-hidden border border-white/20">
+    <div className="flex-shrink-0  bg-white/10 backdrop-blur-md rounded-lg shadow-lg hover:shadow-[0_0_20px_rgba(255,86,246,0.6)] transition-shadow duration-300 overflow-hidden border border-white/20">
       
       {/* Image */}
       {image && (
-        <img
-          src={image}
-          alt={title}
-          className="w-full h-60 object-cover rounded-t-lg"
-        />
+        <div className="w-full h-48 sm:h-56 md:h-60 lg:h-64 overflow-hidden rounded-t-lg">
+          <img
+            src={image}
+            alt={title}
+            className="w-full h-full object-contain"
+          />
+        </div>
       )}
 
       {/* Content */}
-      <div className="p-4 flex flex-col justify-between">
+      <div className="p-4 flex flex-col justify-between h-32">
         <div>
-          <h3 className="text-sm font-semibold text-white">{title}</h3>
-          <p className="text-white/70 mt-1 text-xs">{description}</p>
+          <h3 className="text-sm sm:text-base font-semibold text-white">{title}</h3>
+          <p className="text-white/70 mt-1 text-xs sm:text-sm">{description}</p>
         </div>
       </div>
     </div>
